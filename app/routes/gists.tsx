32097c0db111fab0a08dcb9ba3a5cad4084e7294
Ahmed ElDessouki <nemoahmed@hotmail.co.uk>
2021-11-17 15:54:51 +0300

    Remix-App

---
+++ app/routes/gists.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link, LoaderFunction, Outlet } from "remix";
+import { useLoaderData } from "remix";
+
+export let loader: LoaderFunction = () => {
+  return fetch("https://api.github.com/gists");
+};
+
+export function meta({ data }: { data: any[] }) {
+  return {
+    title: "Public Gists",
+    description: `View the latest ${data.length} gists from the public`,
+  };
+}
+
+export default function Gists() {
+  let data = useLoaderData();
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div>
+      <button onClick={() => setCount((n) => n + 1)}>{count}</button>
+      <h2>Public Gists</h2>
+      <ul>
+        {data.map((gist: any) => (
+          <li key={gist.id}>
+            <Link to={gist.owner.login}>{Object.keys(gist.files)[0]}</Link>
+          </li>
+        ))}
+      </ul>
+      <Outlet />
+    </div>
+  );
+}
---
+++ app/routes/gists/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function GistIndex() {
+  return (
+    <div>
+      <h3>Gist</h3>
+      <p>
+        Here we could show cool stats about the team or recent activity, etc.
+      </p>
+    </div>
+  );
+}
