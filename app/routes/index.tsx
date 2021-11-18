629982a738a5d33ae6cc8cfcc72385d8ae548eb6
Ahmed ElDessouki <nemoahmed@hotmail.co.uk>
2021-11-18 18:57:05 +0300

    font fixed

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -19,7 +19,10 @@ export default function Index() {
   return (
     <div style={{textAlign: 'center', padding: 20}}>
       <h2>Welcome to Remix!</h2>
-      <Link to="/gists"> Gists </Link>
+      <Link className="prose" to="/gists">
+        {' '}
+        Gists{' '}
+      </Link>
 
       <p className="line-clamp-2">
         Et molestiae hic earum repellat aliquid est doloribus delectus. Enim
@@ -29,13 +32,21 @@ export default function Index() {
         molestiae fugit.
       </p>
       <p>
-        <a href="https://docs.remix.run">Check out the docs</a> to get started.
+        <a className="prose" href="https://docs.remix.run">
+          Check out the docs
+        </a>{' '}
+        to get started.
       </p>
       <p>Message from the loader: {data.message}</p>
       <p>
-        <Link to="not-found">Link to 404 not found page.</Link> Clicking this
-        link will land you in your root CatchBoundary component.
+        <Link className="prose" to="not-found">
+          Link to 404 not found page.
+        </Link>{' '}
+        Clicking this link will land you in your root CatchBoundary component.
       </p>
+      <h1 className="prose prose-2xl italic font-black">
+        The quick brown fox jumps over the lazy dog.
+      </h1>
     </div>
   )
 }
